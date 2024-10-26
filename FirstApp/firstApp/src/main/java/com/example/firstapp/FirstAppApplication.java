1c773d98384979a0cfb32a395f5fbc6aa4cf9c87
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-10-26 14:04:01 -0500

    Dependency Injection

--- FirstApp/firstApp/src/main/java/com/example/firstapp/FirstAppApplication.java
+++ FirstApp/firstApp/src/main/java/com/example/firstapp/FirstAppApplication.java
@@ -1,14 +1,20 @@
 package com.example.firstapp;
 
+import com.example.firstapp.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class FirstAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(FirstAppApplication.class, args);
         System.out.println("Hello World!");
+        Person obj = context.getBean(Person.class);
+        obj.code();
     }
 
 }
---
+++ FirstApp/firstApp/src/main/java/com/example/firstapp/Person.java
@@ -0,0 +1,11 @@
+package com.example.firstapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    public void code(){
+        System.out.println("code");
+    }
+}

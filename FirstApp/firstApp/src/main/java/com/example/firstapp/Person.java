80c2ba0e073ef9b5c865754e261a6555473541c9
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2024-10-26 14:09:17 -0500

    Autowired

--- FirstApp/firstApp/src/main/java/com/example/firstapp/Person.java
+++ FirstApp/firstApp/src/main/java/com/example/firstapp/Person.java
@@ -1,11 +1,16 @@
 package com.example.firstapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
 
+    @Autowired
+    Laptop laptop;
+
     public void code(){
+        laptop.compile();
         System.out.println("code");
     }
 }
